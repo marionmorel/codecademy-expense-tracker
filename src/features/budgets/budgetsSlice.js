59af2ffb8a712a1bc8c01111cb6ab86459ceb694
ma-mrl <marion.mrl@live.fr>
2024-04-19 13:21:27 +0200

    create a budgets slice

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 export const CATEGORIES = [
     "housing",
     "food",
@@ -13,29 +15,24 @@ export const CATEGORIES = [
     category: category,
     amount: 0,
   }));
-  
-  export const editBudget = (budget) => {
-    return {
-      type: "budgets/editBudget",
-      payload: budget,
-    };
-  };
-  
-  const budgetsReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case "budgets/editBudget":
-        const newBudgets = state.map((budget) => {
+
+  const budgetsSlice = createSlice({
+    name: 'budgets',
+    initialState: initialState,
+    reducers: {
+      editBudget: (state, action) => {
+        state.map(budget => {
           if (budget.category === action.payload.category) {
-            return action.payload;
+            budget.amount = action.payload.amount
           }
-          return budget;
-        });
-        return newBudgets;
-      default:
-        return state;
+          return budget
+        })
+      }
     }
-  };
+  });
   
   export const selectBudgets = (state) => state.budgets;
-  export default budgetsReducer;
+  export const { editBudget } = budgetsSlice.actions;
+  export default budgetsSlice.reducer;
+
   
